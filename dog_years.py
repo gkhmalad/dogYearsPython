d0a80f34e4bfd16430166550f2ddffd889328195
Giorgi Khmaladze <giorgi.khmaladze.official@gmail.com>
2022-01-29 23:27:06 -0500

    Main commit

---
+++ dog_years.py
@@ -0,0 +1,15 @@
+# Calculates a dogs age in human years by simple convesrsion || Python Version
+
+# The age of the dog
+dogAge = 2
+
+# First 2 dog years is 21 years in human time
+earlyYears = 21
+
+# After 2 years every year is 4 years in human time
+laterYears = (dogAge-2)*4
+
+# Total years
+humanYears = earlyYears+laterYears
+
+print(f"My name is Ruffles! Ruff ruff, I am {humanYears} old in human years.\n")
